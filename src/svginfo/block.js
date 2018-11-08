58660181d980d49557d571b5a0a4a9a9912143fd
Álex Acuña Viera <kyuumeitai@gmail.com>
2018-11-08 18:53:57 -0300

    beta 2

--- src/svginfo/block.js
+++ src/svginfo/block.js
@@ -1,6 +1,15 @@
 import './style.scss';
 import './editor.scss';
 
+const htmlBeautify = require( 'js-beautify' ).html;
+
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+
+require( 'codemirror/mode/xml/xml' );
+require( 'codemirror/mode/javascript/javascript' );
+require( 'codemirror/mode/htmlmixed/htmlmixed' );
+require( 'codemirror/mode/css/css' );
+
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const { MediaUpload } = wp.editor;
@@ -20,6 +29,9 @@ registerBlockType( 'copesa-blocks/svg-infography', {
 		id: {
 			type: 'number',
 		},
+		src: {
+			type: 'string',
+		},
 		width: {
 			type: 'number',
 		},
@@ -37,18 +49,55 @@ registerBlockType( 'copesa-blocks/svg-infography', {
 			attributes: {
 				url,
 				id,
-				// width,
-				// height,
+				src,
 			},
 			setAttributes,
 		} = props;
 
+		let tempcode = '';
+
 		const onSelectImage = ( media ) => {
-			console.log( 'derp' );
+			fetch( media.url )
+				.then( r => r.text() )
+				.then( mediasrc =>{
+					tempcode = mediasrc;
+					setAttributes( {
+						url: media.url,
+						id: media.id,
+						src: mediasrc,
+					} );
+				}
+				);
+		};
+
+		const onChangeCode = ( code ) => {
+			tempcode = code;
+			// setAttributes( {
+			// 	src: code,
+			// } );
+		};
+
+		const codeSave = ( ) => {
 			setAttributes( {
-				url: media.url,
-				id: media.id,
+				src: tempcode,
 			} );
+		};
+
+		const CodemirrorMode = {
+			name: 'htmlmixed',
+			// scriptTypes: [ {
+			// 	matches: /\/x-handlebars-template|\/x-mustache/i,
+			// 	mode: null,
+			// },
+			// {
+			// 	matches: /(text|application)\/(x-)?vb(a|script)/i,
+			// 	mode: 'vbscript',
+			// } ],
+			// tags: {
+			// 	style: [
+			// 		[ null ], [ null, null, 'css' ],
+			// 	],
+			// },
 		};
 
 		return (
@@ -60,11 +109,33 @@ registerBlockType( 'copesa-blocks/svg-infography', {
 						value={ id }
 						render={ ( { open } ) => (
 							<Button className={ id ? 'image-button' : 'button button-large' } onClick={ open } >
-								{ ! id ? __( 'Subir SVG' ) : <img className="svg" src={ url } alt="" /> }
+								{ ! id ? __( 'Subir SVG' ) : <span dangerouslySetInnerHTML={ { __html: src } }></span> }
 							</Button>
 						) }
 					/>
+					{
+						id && (
+							<div>
+								<Button className="button button-large" onClick={ codeSave } >
+									Guardar HTML
+								</Button>
+								<CodeMirror
+									value={ htmlBeautify( src, { indent_size: 2 } ) }
+									options={ {
+										mode: CodemirrorMode,
+										selectionPointer: true,
+										theme: 'material',
+										lineNumbers: true,
+									} }
+									onChange={ ( editor, data, value ) => {
+										onChangeCode( value );
+									} }
+								/>
+							</div>
+						)
+					}
 				</div>
+
 			</div>
 		);
 	},
@@ -73,14 +144,14 @@ registerBlockType( 'copesa-blocks/svg-infography', {
 		const {
 			className,
 			attributes: {
-				url,
+				src,
 			},
 		} = props;
 		return (
 			<div className={ className }>
 				{
-					url && (
-						<img className="svg-image" src={ url } alt="" />
+					src && (
+						<span dangerouslySetInnerHTML={ { __html: src } }></span>
 					)
 				}
 			</div>
